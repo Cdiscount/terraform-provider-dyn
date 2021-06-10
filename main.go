37b4fb5ebce10d710a1f81cde3ba49296d3e522c
Thomas Loubiou <thomas.loubiou@ext.cdiscount.com>
2021-06-10 10:41:50 +0200

    doc: Add generation code

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"gitlab.cshield.io/cshield.tech/infra/terraform-provider-dyn/dyn"
 )
+// Generate docs for website
+//go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
