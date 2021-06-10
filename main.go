32d32d320181370b8117bdcd3f4f22bfbcffd89c
Thomas Loubiou <thomas.loubiou@ext.cdiscount.com>
2021-06-10 18:12:19 +0200

    style: format go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"gitlab.cshield.io/cshield.tech/infra/terraform-provider-dyn/dyn"
 )
+
 // Generate docs for website
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
